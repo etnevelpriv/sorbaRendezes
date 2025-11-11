0ebacfb5c0d8c2178c8d46ea3e4f4412ac1701c7
etnevelpriv <leventerastovits@gmail.com>
2025-11-11 16:58:11 +0100

    Alap css, adatok kiírása

--- szoftverek/weboldal/assets/js/main.js
+++ szoftverek/weboldal/assets/js/main.js
@@ -1,2 +1,27 @@
-let repArray = [];
+"use strict"; // Jobb hibakezelés miatt
+
 let dataArray;
+let dataObj;
+
+const repInit = function () { // Az összes ábrázoláshoz kapcsolatos függvényt ebben hívom meg
+    const repArray = [3, 34, 10, 99, 24, 55, 9];
+    let repContainer = document.getElementById("rep");
+
+    ecsrRep(repArray, repContainer);
+};
+
+const ecsrRep = function (repArray, container) { // Egyszerű Cserés Rendezés függvénye
+    const elements = document.createElement("div");
+    elements.classList.add("rep-elements");
+    container.appendChild(elements);
+
+    repArray.forEach(data => {
+        const element = document.createElement("div");
+        element.classList.add("rep-element");
+        element.textContent = data;
+        elements.appendChild(element);
+    });
+
+};
+
+repInit();
