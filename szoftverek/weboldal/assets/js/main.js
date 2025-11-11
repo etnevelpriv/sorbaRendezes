c9dfc4a8ec63e77c9138ca5ec2e0f4153ad53419
etnevelpriv <leventerastovits@gmail.com>
2025-11-11 17:55:03 +0100

    repInit függvény szerintem teljesen kész. Minden egyes függvényre egyesével lefuttat egy ciklust, így rengeteg sor kódot spórolunk meg a későbbiekben.

--- szoftverek/weboldal/assets/js/main.js
+++ szoftverek/weboldal/assets/js/main.js
@@ -4,24 +4,63 @@ let dataArray;
 let dataObj;
 
 const repInit = function () { // Az összes ábrázoláshoz kapcsolatos függvényt ebben hívom meg
-    const repArray = [3, 34, 10, 99, 24, 55, 9];
-    let repContainer = document.getElementById("rep");
+    console.log("repInit elindult");
 
-    ecsrRep(repArray, repContainer);
+    // Alap változók
+    const repArray = [3, 34, 10, 99, 24, 55, 9];
+    const sortMethods = [ecsSort, buSort, gySort, kSort, beSort];
+    const container = document.getElementById("rep");
+
+    console.log("repInit for ciklus elkezdődött");
+
+    // Ciklus az összes rendezési módszerhez
+    for (let i = 0; i < sortMethods.length; i++) {
+
+        // Tárolók létrehozása
+        const repContainer = document.createElement("div");
+        repContainer.classList.add("rep-container");
+        container.appendChild(repContainer);
+        const elements = document.createElement("div");
+        elements.classList.add("rep-elements");
+        repContainer.appendChild(elements);
+
+        // Tömb minden adatához saját elem
+        repArray.forEach(data => {
+            const element = document.createElement("div");
+            element.classList.add("rep-element");
+            element.textContent = data;
+            elements.appendChild(element);
+        });
+
+        // Függvény futattásához gomb
+        const startButton = document.createElement("button");
+        startButton.classList.add("rep-button");
+        startButton.textContent = "Gomb";
+        startButton.addEventListener("click", () => sortMethods[i](elements)); // Ehez segítséget használtam, mert nem voltam tisztában az addEventListener pontos működésével: https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
+        repContainer.appendChild(startButton);
+    };
+
+    console.log("repInit for ciklus lefutott");
 };
 
-const ecsrRep = function (repArray, container) { // Egyszerű Cserés Rendezés függvénye
-    const elements = document.createElement("div");
-    elements.classList.add("rep-elements");
-    container.appendChild(elements);
+const ecsSort = function (elements) { // Egyszerű Cserés Rendezés
+    console.log("Elso")
+};
 
-    repArray.forEach(data => {
-        const element = document.createElement("div");
-        element.classList.add("rep-element");
-        element.textContent = data;
-        elements.appendChild(element);
-    });
+const buSort = function (elements) { // Buborék rendezés
+    console.log("Masodik")
+};
 
+const gySort = function (elements) { // Gyors rendezés
+    console.log("Harmadik")
+};
+
+const kSort = function (elements) { // Kiválasztásos rendezés
+    console.log("Negyedik")
+};
+
+const beSort = function (elements) { // Beszúrásos rendezés
+    console.log("Otodik")
 };
 
 repInit();
