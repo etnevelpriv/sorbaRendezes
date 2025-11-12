6648e9c5cecbf64f198cf248e68aa117392773f9
etnevelpriv <leventerastovits@gmail.com>
2025-11-12 23:09:39 +0100

    Generalo fuggveny keszen van. Nem kell kulon fuggveny, beraktam az initbe

--- szoftverek/weboldal/assets/js/data.js
+++ szoftverek/weboldal/assets/js/data.js
@@ -1,16 +1,20 @@
-const init = function() {
-    const sortMethods = [ ecsSort, buSort, gySort ];
-    // for ( let i = 0; i < sortMethods.length;i++ ) {
-    //     sortMethods[i]();
-    // };
-    generateArray();
-};
-
-const generateArray = function() {
+const init = function () {
+    const sortMethods = [ecsSort, buSort, gySort];
     const dataArray = [];
-    for ( let i = 0; i < 2; i++ ) {
-        randomSzam = Math.floor(Math.random() * 100000) + 1;
-        console.log(randomSzam);
+    console.log("fuggveny betoltott")
+    for (let i = 1; i < 1000000; i = i * 10) {
+        console.log("For i ciklus lefutott: " + i)
+        dataArray.length = 0;
+        for (let j = 0; j < i * 10; j++) {
+            console.log("For j ciklus lefutott: " + j)
+            randomSzam = Math.floor(Math.random() * 1000000) + 1;
+            dataArray.push(randomSzam)
+        };
+        for (let i = 0; i < sortMethods.length; i++) {
+            for (let f = 0; f < 5; f++) {
+                sortMethods[i]();
+            };
+        };
     };
 };
 
