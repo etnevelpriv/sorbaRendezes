214f32369cb12ff078fafa0c3ade2c6548ad6229
etnevelpriv <leventerastovits@gmail.com>
2025-11-17 21:26:14 +0100

    Időt mérve készen van a 3 alap függvény (css-be i elmentettem)

--- szoftverek/weboldal/assets/js/data.js
+++ szoftverek/weboldal/assets/js/data.js
@@ -1,9 +1,12 @@
+"use strict";
+
 // Generál tömböket és azokat egyből meg is adja paraméternek az összes módszer esetében 5 alkalommal
 const init = function () {
-    const sortMethods = [ecsSort];
+    const sortMethods = [ecsSort, buSort, gySort];
     const dataArray = [];
+    let randomSzam;
     // console.log("fuggveny betoltott")
-    for (let i = 1; i < 100000; i = i * 10) {
+    for (let i = 1; i < 10000; i = i * 10) {
         // console.log("For i ciklus lefutott: " + i)
         dataArray.length = 0;
         for (let j = 0; j < i * 10; j++) {
@@ -32,22 +35,73 @@ const ecsSort = function (dataArray) { // Egyszerű Cserés Rendezés méréssel
                 helper = arr[i];
                 arr[i] = arr[j];
                 arr[j] = helper;
-            }
-        }
-    }
+            };
+        };
+    };
 
     const end = performance.now();
-    const elapsedMs = end - start;
-    console.log(`Egyszerű cserés rendezés ideje ${arr.length} számú elemmel: ${elapsedMs}. Ellenőrzés: első elem: ${arr[0]} és az utolsó elem: ${arr[arr.length-1]}`);
+    const elapsedMs = Number((end - start).toFixed(10));
+    console.log(`Egyszerű cserés rendezés ideje ${arr.length} számú elemmel: ${elapsedMs}. Ellenőrzés: első elem: ${arr[0]} és az utolsó elem: ${arr[arr.length - 1]}`);
 
 };
 
 const buSort = function (dataArray) { // Buborék rendezés
-    console.log("Masodik");
+    const arr = dataArray.slice();
+    let helper;
+    const start = performance.now();
+
+    for (let i = arr.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                helper = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = helper;
+            };
+        };
+    };
+
+    const end = performance.now();
+    const elapsedMs = Number((end - start).toFixed(10));
+    console.log(`Buborékos rendezés ideje ${arr.length} számú elemmel: ${elapsedMs}. Ellenőrzés: első elem: ${arr[0]} és az utolsó elem: ${arr[arr.length - 1]}`);
 };
 
-const gySort = function (dataArray) { // Gyors rendezés
-    console.log("Harmadik");
+const gySort = async function quickSortInside(dataArray) { // Gyors rendezés
+
+    const gySortInside = function gySortInsideRekurzio(dataArray, low, high) { // Gyors rendezés
+        let i = low;
+        let j = high;
+        let helper;
+        const pivotIndex = Math.floor((low + high) / 2);
+        const pivot = dataArray[pivotIndex];
+
+        while (i <= j) {
+            while (dataArray[i] < pivot) {
+                i++;
+            };
+            while (dataArray[j] > pivot) {
+                j--;
+            };
+            if (i <= j) {
+
+                helper = dataArray[i];
+                dataArray[i] = dataArray[j];
+                dataArray[j] = helper;
+
+                i++;
+                j--;
+            };
+        };
+
+        if (low < j) gySortInsideRekurzio(dataArray, low, j);
+        if (i < high) gySortInsideRekurzio(dataArray, i, high);
+    };
+
+    const start = performance.now();
+    gySortInside(dataArray, 0, dataArray.length - 1);
+
+    const end = performance.now();
+    const elapsedMs = Number((end - start).toFixed(10));
+    console.log(`Gyors rendezés ideje ${dataArray.length} számú elemmel: ${elapsedMs}. Ellenőrzés: első elem: ${dataArray[0]} és az utolsó elem: ${dataArray[dataArray.length - 1]}`);
 };
 
 init();
